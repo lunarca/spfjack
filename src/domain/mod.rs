8f99fea28e02969e324f1c70b7d547c819e9c7e4
Alex DeFreese <alexdefreese@gmail.com>
2022-02-16 00:27:00 -0500

    Remove old method of resolving DNS records

--- src/domain/mod.rs
+++
@@ -1,8 +0,0 @@
-use trust_dns_resolver::Resolver;
-
-pub fn domain_registered(resolver: Resolver, domain: String) -> bool {
-    match resolver.ipv4_lookup(domain) {
-        Ok(_) => true,
-        Err(_) => false
-    }
-}
