b571a58e4e8cf9a6f6db2d68cd222a9979b1ce0f
Alex DeFreese <alexdefreese@gmail.com>
2022-02-16 22:47:14 -0500

    Refactor errors for spf fetching functions

--- src/spf/mod.rs
+++ src/spf/mod.rs
@@ -1,31 +1,37 @@
 
-use decon_spf::spf::Spf;
+use decon_spf::spf::{Spf, SpfErrorType};
 use trust_dns_resolver::Resolver;
 use trust_dns_resolver::lookup::TxtLookup;
 
 pub mod processing_results;
 
-pub fn fetch_and_parse(resolver: Resolver, domain: String) -> Result<Spf, String> {
+pub fn fetch_and_parse(resolver: Resolver, domain: String) -> Result<Spf, SpfFetchError> {
   fetch_txt_records(resolver, domain)
     .and_then(select_spf_record)
 }
 
-fn fetch_txt_records(resolver: Resolver, domain: String) -> Result<TxtLookup, String> {
+fn fetch_txt_records(resolver: Resolver, domain: String) -> Result<TxtLookup, SpfFetchError> {
   match resolver.txt_lookup(domain) {
-    Err(_) => Err(String::from("No TXT records")),
+    Err(_) => Err(SpfFetchError::NoTxtRecords),
     Ok(txt_record) => Ok(txt_record)
   }
 }
 
-fn select_spf_record(txt_records: TxtLookup) -> Result<Spf, String> {
+fn select_spf_record(txt_records: TxtLookup) -> Result<Spf, SpfFetchError> {
   for record in txt_records.iter() {
     if record.to_string().starts_with("v=spf1") {
       let mut record = Spf::from_str(&record.to_string());
       return match record.parse() {
         Ok(_) => Ok(record),
-        Err(_) => Err(String::from("Error decoding SPF record"))
+        Err(error) => Err(SpfFetchError::SpfParseError(error))
       }
     }
   }
-  return Err(String::from("No SPF record"))
+  return Err(SpfFetchError::NoSpfRecords)
+}
+
+pub enum SpfFetchError {
+  NoTxtRecords,
+  NoSpfRecords,
+  SpfParseError(SpfErrorType)
 }
