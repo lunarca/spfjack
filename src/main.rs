b9cfb326beae4027f90c2fb66524c20d566ef067
Alex DeFreese <alexdefreese@gmail.com>
2022-03-27 15:50:22 -0400

    Boilerplate to interact with new SPF fetching code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ mod spf;
 mod dns;
 
 use clap::{Arg, App, ArgMatches};
+use decon_spf::Spf;
 
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+
+use dns::dns_resolver::{
+    new_resolver,
+    resolve_spf_record,
+};
+
+pub type SpfCache = Arc<Mutex<HashMap<String, Arc<Spf>>>>;
 
 #[tokio::main]
 async fn main() {
@@ -11,7 +21,15 @@ async fn main() {
 
     let domain = String::from(args_matcher.value_of("domain").unwrap());
 
-    let resolver = dns::dns_resolver::new_resolver();
+    println!("Targeting domain: {}", domain);
+
+    let cache: SpfCache = Arc::new(Mutex::new(HashMap::new()));
+
+    let resolver = new_resolver();
+
+    let record = resolve_spf_record(&domain, &resolver, cache).await.unwrap();
+
+    println!("Fetched record: {}", record.to_string());
 
 }
 
