f85d601983ceba1864c6524f52947090cd16696b
Alex DeFreese <alexdefreese@gmail.com>
2022-03-27 14:59:42 -0400

    Remove Actix and actors

--- src/actors/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod spf_cache;
-pub mod resolver;
-pub mod spf_mechanism_processor;
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,13 @@
 mod spf;
-mod actors;
 
-use actix::System;
 use clap::{Arg, App, ArgMatches};
 
-use actors::spf_cache;
-use actors::resolver;
 
-
-#[actix::main]
+#[tokio::main]
 async fn main() {
 
     let args_matcher = parse_args();
 
-    let spf_cache_addr = spf_cache::start_link();
-
-    let resolver_addr = resolver::start_link(&spf_cache_addr);
-
-    let domain: String = String::from(args_matcher.value_of("domain").unwrap());
-
-
-    let spf_record = resolver_addr.send(resolver::FetchSfpRecordMessage{dns_name: domain}).await.unwrap();
-    match spf_record {
-        Ok(record) => println!("SPF Record: {}", record.source()),
-        Err(_error) => println!("Error")
-    };
-
-
-    System::current().stop();
 
 }
 
