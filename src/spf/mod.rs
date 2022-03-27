43512ef6e66dfc3215c8bb1cb9e5722399ad294f
Alex DeFreese <alexdefreese@gmail.com>
2022-03-27 16:16:02 -0400

    Clean up whitespace in identified SPF records
    Decon-spf now throws a Parse Error when the record has any extra whitespace between mechanisms. Since most SPF processers don't seem to care about this, we'll go ahead and clean up the whitespace so we can actually process this

--- src/spf/mod.rs
+++ src/spf/mod.rs
@@ -1,6 +1,7 @@
 use decon_spf::{Spf, SpfError};
 use trust_dns_resolver::TokioAsyncResolver;
 use trust_dns_resolver::lookup::TxtLookup;
+use regex::Regex;
 
 pub mod processing_results;
 
@@ -20,7 +21,8 @@ async fn fetch_txt_records(resolver: &TokioAsyncResolver, domain: String) -> Res
 fn select_spf_record(txt_records: TxtLookup) -> Result<Spf, SpfFetchError> {
   for record in txt_records.iter() {
     if record.to_string().starts_with("v=spf1") {
-      let record_result: Result<Spf, SpfError>  = record.to_string().parse();
+      let record_result: Result<Spf, SpfError>  = clean_up_record_whitespace(record.to_string())
+        .parse();
       return match record_result {
         Ok(record) => Ok(record),
         Err(error) => Err(SpfFetchError::SpfParseError(error))
@@ -35,4 +37,10 @@ pub enum SpfFetchError {
   NoTxtRecords,
   NoSpfRecords,
   SpfParseError(SpfError)
+}
+
+fn clean_up_record_whitespace(txt_record: String) -> String {
+  let re = Regex::new(r"\s+").unwrap();
+  let text = re.replace_all(&txt_record, " ");
+  text.to_string()
 }
