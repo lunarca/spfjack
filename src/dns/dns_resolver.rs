5a9367521e9749c6d5631083819bb2c545a4e150
Alex DeFreese <alexdefreese@gmail.com>
2022-03-29 20:57:08 -0400

    Add logging to DNS functions

--- src/dns/dns_resolver.rs
+++ src/dns/dns_resolver.rs
@@ -22,14 +22,19 @@ pub async fn resolve_spf_record(domain: &String, resolver: &TokioAsyncResolver, 
 
     match cache.get(domain) {
         // Cache hit: Just return the resulting SPF record
-        Some(record) => Ok(Arc::clone(record)),
+        Some(record) => {
+            debug!("Cache hit: {}", domain);
+            Ok(Arc::clone(record))
+        },
 
         // Cache miss: Fetch the SPF record, then add to cache
         None => {
+            debug!("Cache miss: {}", domain);
             let optional_spf_record = spf::fetch_and_parse(resolver, domain.clone())
                 .await
                 .map(|record| Arc::new(record))
                 .and_then(|record| {
+                    debug!("Got record for domain {}, adding to cache: {}", domain, record.to_string());
                     cache.insert(domain.clone(), Arc::clone(&record));
                     Ok(record)
                 });
@@ -40,9 +45,11 @@ pub async fn resolve_spf_record(domain: &String, resolver: &TokioAsyncResolver, 
 }
 
 pub async fn is_domain_registered(resolver: &TokioAsyncResolver, domain: &String) -> bool {
+    trace!("is_domain_registered on {}", domain);
     match resolver.ns_lookup(domain).await {
         Ok(_) => true,
         Err(err) => {
+            debug!("Resolve error: {:?}", err);
             match err.kind() {
                 ResolveErrorKind::NoRecordsFound {  response_code, ..} if *response_code == ResponseCode::NXDomain => {
                     false
